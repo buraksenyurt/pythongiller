a90b94d1506d080a914c164e25f94fa83e8c16d3
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-11-18 22:43:59 +0300

    some code fix

--- Game08_FactorFinder/carpbul.py
+++ Game08_FactorFinder/carpbul.py
@@ -1,6 +1,8 @@
 import math, random, sys, time
 from termcolor import COLORS, colored
 
+# Bekleme efektinde kullanacağımız karakterler.
+# Sankin dönen bir çarkmış hissiyatı uyandırmak için
 WAIT_EFFECT = ['-', '\\', '|', '/', '-', '|', '\\']
 
 
@@ -12,11 +14,11 @@ def main():
         Sevgili matematik sever hoşgeldin. Benimle güzel bir oyuna var mısın? :)
         Sana 1 ile 100 arasında bir sayı söyleyeceğim ve çarpanlarını bulup yazmanı isteyeceğim.
 
-            Örneğin 26 sayısı için
+        Örneğin 26 sayısı için
 
             1,2,13,16
 
-        yazıp enter'la.
+            yazıp enter'la.
         
         Eğer sıkılırsan HOF POF yaz.
 
@@ -24,31 +26,40 @@ def main():
     ''', "yellow"))
     input()
 
+    # 1 ile 100 arası rastgele sayı üretiyoruz
+    number = random.randint(1, 100)
+
     # Oyunun olmazsa olmaz sonsuz döngüsü
     while True:
-        number = random.randint(1, 100)
+        print(colored('Sayımız : {}'.format(number), 'green'))
+        # oyuncunun tahminini istiyoruz
         player_guess = input('..:')
 
+        # Önce oyuncunun girdiği bilgiyi kontrol ediyoruz. HOF POF yazarsa sisteme çıkıyoruz.
         if player_guess.upper() == "HOF POF":
             print(colored('Yine gelllll', 'yellow'))
             sys.exit()
 
+        # Bilgisayar o sırada çarpanları buluyor
         factors = findFactors(number)
+        # Formatlı halini de hesaplatalım.
+        formated_factors = displayFactors(factors)
         print('İzninle bende çarpanları hesap edeceğim')
+        doWaitEffect()
 
-        for c in WAIT_EFFECT:
-            time.sleep(1)
-            print(colored(c, 'magenta', attrs=["bold"]))
-
-        if factors == player_guess:
+        # Oyuncusun girdiği sayı ile programın hesapladığı ifadeyi kıyaslıyoruz.
+        # İfadeler doğruysa programdan çıkıyoruz.
+        if formated_factors == player_guess:
             print(colored('BİLDİN!!!\a', 'green'))
+            sys.exit()
         else:
             print(
                 colored(
                     '''
-            
-                Üzgünüm ama bilemedin...
-                Yeniden denemek ister misin? (E/H)                
+
+        Üzgünüm ama bilemedin...
+        Yeniden denemek ister misin? (E/H)  
+
         ''', 'red'))
             response = input('..:')
             if response.upper() == 'E':
@@ -57,9 +68,13 @@ def main():
                 print(
                     colored(
                         '''
-                Öyleyse sana çarpanları söyleyeyim
-                {}
-                '''.format(factors), 'yellow'))
+        Öyleyse sana çarpanları söyleyeyim
+        {}
+
+        Benimle oynadığın için teşekkürler ;)
+                
+                '''.format(formated_factors), 'yellow'))
+                sys.exit()
 
 
 '''
@@ -84,6 +99,7 @@ def findFactors(number):
     factors = list(set(factors))
     # fonksiyondan döndüğümüz içerikse listenin sıralanmış hali.
     factors.sort()
+    return factors
 
 
 '''
@@ -99,7 +115,22 @@ def displayFactors(factors):
         # buna göre i indisli alan f'nin string dönüştürülmüş hali ekleniyor.
         factors[i] = str(f)
     # join ile factors içerisindeki elemanları birer virgül ekleyip birbirlerine bağlıyor ve ekrana yazdırıyoruz.
-    print(','.join(factors))
+    return ','.join(factors)
+
+
+'''
+    Program düşünürken komut satırında WAIT_EFFECT içindeki karakterler bir çark gibi dönecek ve bekleme efekti uygulayacak ;)
+'''
+
+
+def doWaitEffect():
+    # Burada sembolik olarak bir bekleme efekti yapıyoruz.
+    # Wait effect'teki karakterleri basıp satır başına döndürüyoruz.
+    # Tabi aynı işlemi 3 kere tekrar ettiriyoruz.
+    for _ in range(1, 3):
+        for c in WAIT_EFFECT:
+            time.sleep(0.5)  # yarım saniyelik bekletme
+            print(colored(c, 'magenta', attrs=["bold"]), end="\r")
 
 
 if __name__ == '__main__':
