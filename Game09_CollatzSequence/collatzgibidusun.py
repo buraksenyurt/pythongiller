81a1dc6c5f61137f598df5459a3bd852dcf79306
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-11-23 18:04:23 +0300

    code complete.

--- Game09_CollatzSequence/collatzgibidusun.py
+++ Game09_CollatzSequence/collatzgibidusun.py
@@ -54,36 +54,57 @@ def start():
         
         '''.format(number), "yellow"))
     # Sembolik bir bekletme efekti
-    doWaitEffect()
+    wait()
     # Bekleme sonrası program collatz serisini bulacak ve hem seriyi hem de kaç seferde 1 e gelindiğini geriye döndürecek.
+    # Ödev: Bu fonksiyonun doWaitEffect ile paralel çalışması nasıl sağlanır?
     count, series = find_collatz(number)
     # oyuncunun tahminini alıyoruz
     player_guess = input(colored("Tahminini alabilir miyim?..: ", "yellow"))
     # öncelikle girilen tahminin sayısal bir değer olup olmadığını veya 0 olup olmadığına bakıyoruz.
     if not player_guess.isnumeric() or player_guess == '0':
+        # /a normal şartlarda sistem beep sesini veriyor
         print(
-            # /a normal şartlarda sistem beep sesini veriyor
             colored("0 dan farklı pozitif bir tahminde bulunmalıydın\a",
                     "red"))
+        sys.exit()  # sisteme geri dön.
+
     # oyuncunun tahminine göre bir cevap verilip collatz serisi ekrana basılıyor.
-    if player_guess == count:
-        print(colored('Bravo bildin :)', "yellow"))
+    if int(player_guess) == count:
+        print(
+            colored(
+                'Bravo bildin :) Tam {} seferde bire ulaşılıyor.\n'.format(
+                    count), "yellow"))
     else:
-        print(colored('Üzgünüm. Sayı dizisi şöyle.', "yellow"))
+        print(colored('Üzgünüm :( Doğru cevap {}\n'.format(count), "yellow"))
 
     for n in series:
-        print(colored('{},'.format(n), 'green'))
+        print(colored('{},'.format(n), 'green'), end='')
 
 
 # fonksiyon collatz serisini parametre olarak gelen sayı içim bulur.
 # Ayrıca kaç hamelde 1'e ulaşıldığını da geriye döndürür.
 def find_collatz(number):
-    return 1, [1, 2, 3, 4]
+    # geriye collatz serisini döndürmek için bir dizi kullanabiliriz.
+    series = []
+    n = int(number)
+    count = 1
+    series.append(n)
+    # 1 sayısına gelininceye dek hesaplamaları yapıyoruz
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = (n * 3) + 1
+        series.append(n)
+        count += 1  # hamle sayısını toplayarak ilerliyoruz
+
+    # Fonksiyon iki değer döndürüyor.
+    return count, series
 
 
 # carpbul.py uygulamasında kullandığımız bekletme efektini burada da kullanabiliriz
 # 20 saniyeye tekabül eden bir bekletme söz konusu
-def doWaitEffect():
+def wait():
     for _ in range(1, 3):
         for c in ['-', '\\', '|', '/', '-', '|', '\\']:
             time.sleep(0.5)  # yarım saniyelik bekletme
