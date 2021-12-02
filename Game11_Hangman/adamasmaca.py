678712da6ef4fe3fa139f0e589ff994554069785
Burak Selim Şenyurt <burakselimsenyurt@gmail.com>
2021-12-02 19:02:58 +0300

    some changes

--- Game11_Hangman/adamasmaca.py
+++ Game11_Hangman/adamasmaca.py
@@ -1,5 +1,6 @@
 import random, sys
 from termcolor import COLORS, colored
+import json  # Json okumaları için kullanacağız
 
 HANGMAN_PICS = [
     r"""
@@ -54,6 +55,8 @@ HANGMAN_PICS = [
 ====="""
 ]
 
+COUNTRY = "Türkiye"
+
 
 def main():
     print(
@@ -63,7 +66,36 @@ def main():
 
         Hazır olduğunda bir tuşa bas ;)
     """, "cyan"))
+
     input(colored('..:', 'cyan'))
+
+    countries = getCountryNames()
+    print(countries)
+
+
+'''
+Json veri dosyasından ülke adlarını alan fonksiyondur.
+Geriye ülke adlarından oluşan listeyi döndürür.
+'''
+
+
+def getCountryNames():
+    names = []
+    # cities.json dosyası utf8 formatlı olarak açılır(Türkçe karakter desteği olsun diye)
+    file = open('cities.json', encoding='utf8')
+    # dosyadaki veriler json paketinin load fonksiyonu ile yüklenir.
+    data = json.load(file)
+
+    # countries json içeriğinde dolaşılır
+    for i in data['countries']:
+        # name alanı değerleri names listesine eklenir
+        names.append(i["name"])
+
+    # açılmış dosya kapatılır
+    file.close()
+
+    # elde edilen ülke adları geriye döndürülür.
+    return names
 
 
 if __name__ == '__main__':
